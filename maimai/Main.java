66385f7109fa02d821bc086cd0966ce362a67f3b
José Antônio Rodrigues Santos <antonio.rodrigues@dcomp.ufs.br>
2026-06-10 20:13:58 -0300

    Delete maimai directory

--- maimai/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // 1. Cria a Central (que instancia todas as telas automaticamente pelo construtor)
-        Central app = new Central();
-        app.VerTelaPrimaria(true); // Faz o setVisible(true)
-    }
-}
